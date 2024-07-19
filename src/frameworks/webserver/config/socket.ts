5aa189c489d0027348dd149314c496802a7b1fbf
anandms <anandsathyan19@gmail.com>
2024-07-19 12:41:12 +0530

    socket origin

--- src/frameworks/webserver/config/socket.ts
+++ src/frameworks/webserver/config/socket.ts
@@ -18,7 +18,7 @@ export default function initializeSocket(server: HTTPServer): Server {
     const io = new Server(server, {
         pingTimeout: 60000,
         cors: {
-            origin: "http://localhost:5173",
+            origin: process.env.ORIGIN,
         },
     });
 
